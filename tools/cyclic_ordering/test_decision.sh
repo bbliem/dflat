3d640ce0fadf363a9bc786716f587b7adce819b6
Bernhard Bliem <bliem@dbai.tuwien.ac.at>
2012-10-06 13:49:03 +0000

    Adapted ASP encodings to current D-FLAT version and improved them.
    git-svn-id: https://sorr.dbai.tuwien.ac.at/repos/dflat-source/trunk@94 ae4977aa-0cbe-11e2-8036-c3716e694b28

--- tools/cyclic_ordering/test_decision.sh
+++ tools/cyclic_ordering/test_decision.sh
@@ -4,6 +4,7 @@ numElements=10
 numOrderings=14
 export instanceGen="tools/cyclic_ordering/instance_generator.py $numElements $numOrderings"
 export monolithicEncoding=asp_encodings/cyclic_ordering/monolithic.lp
-export dflatArguments="-x asp_encodings/cyclic_ordering/exchange_decision.lp -e order -n semi"
+#export dflatArguments="-x asp_encodings/cyclic_ordering/exchange_decision.lp -e order -n semi"
+export dflatArguments="asp_encodings/cyclic_ordering/dynamic_decision.lp -e order"
 
 tools/test_decision.sh
