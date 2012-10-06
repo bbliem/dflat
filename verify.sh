87877899748ab121e9806c41c800247cc22f70c1
Bernhard Bliem <bliem@dbai.tuwien.ac.at>
2012-10-06 13:46:34 +0000

    Added script that verifies whether SAT output corresponds to MiniSat's
    git-svn-id: https://sorr.dbai.tuwien.ac.at/repos/dflat-source/trunk@8 ae4977aa-0cbe-11e2-8036-c3716e694b28

---
+++ verify.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+numInstances=100
+numClauses=12
+numVars=8
+
+satgen=../satgen.py
+minisat=/home/bernhard/Informatik/MiniSat_v1.14_linux
+sat=./build/release/sat
+
+for instance in $(seq 1 $numInstances); do
+	seed=$RANDOM
+
+	lpInstance=$(mktemp)
+	dimacsInstance=$(mktemp)
+	trap "rm -f $lpInstance $dimacsInstance" EXIT
+
+	$satgen $numClauses $numVars dimacs $seed > $dimacsInstance
+	$minisat < $dimacsInstance &>/dev/null
+	miniSatExitCode=$?
+
+	$satgen $numClauses $numVars lp $seed > $lpInstance
+	$sat -p decision -s $seed < $lpInstance &>/dev/null
+	satExitCode=$?
+
+	if [[ ($miniSatExitCode -eq 10 && $satExitCode -ne 0) || ($miniSatExitCode -eq 20 && $satExitCode -ne 23) ]]; then
+		cp $dimacsInstance mismatch${seed}.dimacs
+		cp $lpInstance mismatch${seed}.lp
+		echo
+		echo Mismatch for seed $seed
+	else
+		echo -n .
+	fi
+
+	# remove temp file
+	rm -f $dimacsInstance $lpInstance
+	trap - EXIT
+done
+echo
