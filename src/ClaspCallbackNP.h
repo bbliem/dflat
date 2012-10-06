e870896274a5ea9d02099a633e36644fe83b7d60
Bernhard Bliem <bliem@dbai.tuwien.ac.at>
2012-10-06 13:47:51 +0000

    Added GPL
    git-svn-id: https://sorr.dbai.tuwien.ac.at/repos/dflat-source/trunk@51 ae4977aa-0cbe-11e2-8036-c3716e694b28

--- src/ClaspCallbackNP.h
+++ src/ClaspCallbackNP.h
@@ -1,3 +1,23 @@
+/*
+Copyright 2012, Bernhard Bliem
+WWW: <http://dbai.tuwien.ac.at/research/project/dynasp/dflat/>.
+
+This file is part of D-FLAT.
+
+D-FLAT is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+D-FLAT is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with D-FLAT.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
 #include <clasp/clasp_facade.h>
