8e93f36a50f5947bd46b51ea457852e261d87aad
Bernhard Bliem <bliem@dbai.tuwien.ac.at>
2012-10-06 13:48:59 +0000

    Added dynamic program for SAT and fixed error in SAT test script
    git-svn-id: https://sorr.dbai.tuwien.ac.at/repos/dflat-source/trunk@92 ae4977aa-0cbe-11e2-8036-c3716e694b28

--- tools/sat/instance_generator.py
+++ tools/sat/instance_generator.py
@@ -12,6 +12,10 @@ if len(sys.argv) not in (3, 4, 5) or (dimacs and sys.argv[4] != "--dimacs"):
 numClauses = int(sys.argv[1])
 numVars = int(sys.argv[2])
 
+if numClauses < numVars:
+	sys.stderr.write("must have at least as many clauses as variables")
+	sys.exit(1)
+
 if len(sys.argv) >= 4:
 	random.seed(int(sys.argv[3]))
 
@@ -23,9 +27,12 @@ else:
 	for i in range(numVars):
 		print("atom(a{}).".format(i))
 
-
 for i in range(numClauses):
 	atoms = random.sample(range(numVars), random.randint(1,numVars))
+
+	# each var must be in some clause
+	if i < numVars and i not in atoms:
+		atoms.append(i)
 
 	for j in atoms:
 		negative = random.random() < 0.5
--- tools/sat/test_counting.sh
+++ tools/sat/test_counting.sh
@@ -4,6 +4,7 @@ numClauses=12
 numVars=8
 export instanceGen="tools/sat/instance_generator.py $numClauses $numVars"
 export monolithicEncoding=asp_encodings/sat/monolithic.lp
-export dflatArguments="-x asp_encodings/sat/exchange.lp -j asp_encodings/sat/join.lp -e pos -e neg -n semi"
+#export dflatArguments="-x asp_encodings/sat/exchange.lp -j asp_encodings/sat/join.lp -e pos -e neg -n semi"
+export dflatArguments="asp_encodings/sat/dynamic.lp -e pos -e neg"
 
 tools/test_counting.sh
