7e1766a7c01c19b08ceb91c8de1e20256ab1dc8a
Bernhard Bliem <bliem@dbai.tuwien.ac.at>
2012-10-06 13:49:12 +0000

    Changed Hamiltonian Cycle encoding such that it now uses only one level
    git-svn-id: https://sorr.dbai.tuwien.ac.at/repos/dflat-source/trunk@100 ae4977aa-0cbe-11e2-8036-c3716e694b28

--- tools/hamiltonian_cycle/test_counting.sh
+++ tools/hamiltonian_cycle/test_counting.sh
@@ -5,6 +5,6 @@ numEdges=18
 export instanceGen="tools/graphs/instance_generator.py $numNodes $numEdges"
 #export instanceGen="tools/graphs/tree_instance_generator.py $numNodes $numEdges"
 export monolithicEncoding=asp_encodings/hamiltonian_cycle/monolithic.lp
-export dflatArguments="-e edge --multi-level asp_encodings/hamiltonian_cycle/dynamic.lp"
+export dflatArguments="-e edge asp_encodings/hamiltonian_cycle/dynamic.lp"
 
 tools/test_counting.sh
