7f3c8dc3d30e232820db5d9e8ea927e468705673
Bernhard Bliem <bliem@dbai.tuwien.ac.at>
2013-03-05 17:47:42 +0000

    Updated copyright notice for 2013
    git-svn-id: https://sorr.dbai.tuwien.ac.at/repos/dflat-source/trunk@115 ae4977aa-0cbe-11e2-8036-c3716e694b28

--- src/ClaspInputReader.cpp
+++ src/ClaspInputReader.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright 2012, Bernhard Bliem
+Copyright 2012-2013, Bernhard Bliem
 WWW: <http://dbai.tuwien.ac.at/research/project/dynasp/dflat/>.
 
 This file is part of D-FLAT.
--- src/EnumerationIterator.h
+++ src/EnumerationIterator.h
@@ -1,6 +1,6 @@
 
 /*
-Copyright 2012, Bernhard Bliem
+Copyright 2012-2013, Bernhard Bliem
 WWW: <http://dbai.tuwien.ac.at/research/project/dynasp/dflat/>.
 
 This file is part of D-FLAT.
