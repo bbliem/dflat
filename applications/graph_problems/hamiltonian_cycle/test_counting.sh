19f279036ed617d753402c38801c4820a1560c72
Bernhard Bliem <bliem@dbai.tuwien.ac.at>
2014-01-21 15:31:52 +0100

    Adapted Hamiltonian Cycle encodings to gringo 4 syntax

--- applications/graph_problems/hamiltonian_cycle/test_counting.sh
+++ applications/graph_problems/hamiltonian_cycle/test_counting.sh
@@ -5,6 +5,6 @@ numEdges=18
 export instanceGen="$DIR/../instance_generator.py $numNodes $numEdges"
 #export instanceGen="$DIR/../tree_instance_generator.py $numNodes $numEdges"
 export monolithicEncoding=$DIR/monolithic.lp
-export dflatArguments="-e edge $DIR/dynamic.lp"
+export dflatArguments="--tables -e edge -p $DIR/dynamic.lp"
 
 $DIR/../../test_counting.sh
