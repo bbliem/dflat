1c5de83fadce9ce246ab8bad7435bec9d9ab324b
Bernhard Bliem <bliem@dbai.tuwien.ac.at>
2014-02-04 13:59:01 +0100

    Following symlinks in run-dflat.sh.
    Thanks to Michael.

--- run-dflat.sh
+++ run-dflat.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-here=`dirname "$0"`
+bin=`readlink -f "$0"`
+here=`dirname "$bin"`
 #LD_LIBRARY_PATH=$here/lib:$LD_LIBRARY_PATH
 #export LD_LIBRARY_PATH
-#exec "$0".bin "$@"
-exec "$here"/lib/ld-linux*.so* --library-path "$here"/lib "$0".bin "$@"
+#exec "$bin".bin "$@"
+exec "$here"/lib/ld-linux*.so* --library-path "$here"/lib "$bin".bin "$@"
