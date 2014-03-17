aa32e04edf29e7f58c841e7158da16b9baa86cb2
Bernhard Bliem <bliem@dbai.tuwien.ac.at>
2014-03-17 23:52:43 +0100

    Added rudimentary checks on ASP encoding (not in table mode for now)

---
+++ src/solver/asp/trees/EncodingChecker.h
@@ -0,0 +1,44 @@
+/*{{{
+Copyright 2012-2014, Bernhard Bliem
+WWW: <http://dbai.tuwien.ac.at/research/project/dflat/>.
+
+This file is part of D-FLAT.
+
+D-FLAT is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+D-FLAT is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with D-FLAT.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+//}}}
+#include "../EncodingChecker.h"
+
+namespace solver { namespace asp { namespace trees {
+
+class EncodingChecker : public ::solver::asp::EncodingChecker
+{
+public:
+	using ::solver::asp::EncodingChecker::EncodingChecker;
+
+	virtual void showsig(Gringo::Location const &loc, Gringo::FWString name, unsigned arity, bool csp);
+	virtual void check();
+
+private:
+	bool showUsed = false;
+	bool lengthShown = false;
+	bool itemShown = false;
+	bool extendShown = false;
+	bool orAndShown = false;
+	bool acceptRejectShown = false;
+};
+
+}}} // namespace solver::asp::trees
