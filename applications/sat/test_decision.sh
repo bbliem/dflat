511e9739b7ef79faf8f5f36fafff6f212a9260f3
Bernhard Bliem <bliem@dbai.tuwien.ac.at>
2014-03-26 14:15:32 +0100

    Updated encodings for SAT and Cyclic Ordering. Removed metasp.

--- applications/sat/test_decision.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DIR=$(cd "$( dirname "$0" )" && pwd)
-numClauses=12
-numVars=6
-export instanceGen="$DIR/instance_generator.py $numClauses $numVars"
-export monolithicEncoding=$DIR/monolithic.lp
-export dflatArguments="-p $DIR/dynamic.lp"
-
-$DIR/../test_decision.sh
