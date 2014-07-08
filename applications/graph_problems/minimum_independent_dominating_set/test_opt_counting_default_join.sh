be362109875149e6792fa4f4545c269f0233a08a
Bernhard Bliem <bliem@dbai.tuwien.ac.at>
2014-07-08 14:39:24 +0200

    Added encoding for Minimum Independent Dominating Set

---
+++ applications/graph_problems/minimum_independent_dominating_set/test_opt_counting_default_join.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DIR=$(cd "$( dirname "$0" )" && pwd)
+numNodes=10
+numEdges=20
+export instanceGen="$DIR/../instance_generator.py $numNodes $numEdges"
+export monolithicEncoding=$DIR/monolithic.lp
+export dflatArguments="-p $DIR/exchange.lp"
+
+$DIR/../../test_opt_counting.sh
