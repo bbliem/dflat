c9097b983e10b7d78723920852ee698167589dd4
Bernhard Bliem <bliem@dbai.tuwien.ac.at>
2015-02-24 12:36:32 +0100

    More tests for hard-coded ASP solver

--- test/IntegrationAspSolver.cpp
+++ test/IntegrationAspSolver.cpp
@@ -26,7 +26,7 @@ namespace {
 	// http://stackoverflow.com/questions/5419356/redirect-stdout-stderr-to-a-string
 	struct RedirectionGuard
 	{
-		RedirectionGuard(std::ios& oldStream, std::ios& newStream)
+		RedirectionGuard(std::ios& oldStream, const std::ios& newStream)
 			: oldStream(oldStream), oldBuffer(oldStream.rdbuf(newStream.rdbuf()))
 		{}
 
@@ -39,40 +39,153 @@ namespace {
 		std::ios& oldStream;
 		std::streambuf* oldBuffer;
 	};
+
+	std::string runSolver(const std::string& instance)
+	{
+		// Redirect stdin and stdout
+		std::istringstream instanceStream(instance);
+		std::ostringstream newCout;
+		RedirectionGuard coutGuard(std::cout, newCout);
+		RedirectionGuard cinGuard(std::cin, instanceStream);
+
+		Application app("test");
+		const char* argv[] = {"-s", "asp", "-e", "atom", "-e", "rule", "-e", "head", "-e", "pos", "-e", "neg", "-n", "normalized", "--seed", "0", "--output", "quiet", "--depth", "1"};
+		app.run(20, argv);
+		return newCout.str();
+	}
 }
 
 TEST(IntegrationAspSolver, TestProgram1)
 {
-	std::istringstream instance(R"(
-		% a | b.
-		% c :- b.
-		% d :- not c.
-		rule(r1).
-		rule(r2).
-		rule(r3).
+	std::string result = runSolver(R"(
 		atom(a).
 		atom(b).
 		atom(c).
 		atom(d).
+		rule(r1).
+		rule(r2).
+		rule(r3).
+		% a | b.
 		head(r1,a).
 		head(r1,b).
+		% c :- b.
 		head(r2,c).
 		pos(r2,b).
+		% d :- not c.
 		head(r3,d).
 		neg(r3,c).
 	)");
 
-	// Redirect stdin and stdout
-	std::ostringstream newCout;
-	RedirectionGuard coutGuard(std::cout, newCout);
-	RedirectionGuard cinGuard(std::cin, instance);
-
-	Application app("test");
-	const char* argv[] = {"-s", "asp", "-e", "atom", "-e", "rule", "-e", "head", "-e", "pos", "-e", "neg", "-n", "normalized", "--seed", "0", "--output", "quiet", "--depth", "1"};
-	app.run(20, argv);
 	EXPECT_EQ("Solutions:\n"
 	          "\n"
 	          "┣━ [1] a d r1 r2 r3 \n"
 	          "┗━ [1] b c r1 r2 r3 \n",
-	          newCout.str());
+	          result);
+}
+
+TEST(IntegrationAspSolver, TestProgram2)
+{
+	std::string result = runSolver(R"(
+		atom(a).
+		atom(b).
+		atom(c).
+		atom(d).
+		atom(e).
+		rule(r0).
+		rule(r1).
+		rule(r2).
+		rule(r3).
+		rule(r4).
+		% a.
+		head(r0,a).
+		% b :- not c, not e.
+		head(r1,b).
+		neg(r1,c).
+		neg(r1,e).
+		% c :- not b, not e.
+		head(r2,c).
+		neg(r2,b).
+		neg(r2,e).
+		% d :- not e.
+		head(r3,d).
+		neg(r3,e).
+		% e :- not d.
+		head(r4,e).
+		neg(r4,d).
+	)");
+
+	EXPECT_EQ("Solutions:\n"
+	          "\n"
+	          "┣━ [1] a c d r0 r1 r2 r3 r4 \n"
+	          "┣━ [1] a b d r0 r1 r2 r3 r4 \n"
+	          "┗━ [1] a e r0 r1 r2 r3 r4 \n",
+	          result);
+}
+
+TEST(IntegrationAspSolver, TestProgram3)
+{
+	std::string result = runSolver(R"(
+		atom(a).
+		atom(b).
+		rule(r0).
+		rule(r1).
+		rule(r2).
+		% a | b
+		head(r0,a).
+		head(r0,b).
+		% b :- a.
+		head(r1,b).
+		pos(r1,a).
+		% a :- b.
+		head(r2,a).
+		pos(r2,b).
+	)");
+
+	EXPECT_EQ("Solutions:\n"
+	          "\n"
+	          "┗━ [1] a b r0 r1 r2 \n",
+	          result);
+}
+
+TEST(IntegrationAspSolver, TestProgram4)
+{
+	std::string result = runSolver(R"(
+		atom(a).
+		atom(b).
+		atom(c).
+		atom(d).
+		atom(e).
+		rule(r0).
+		rule(r1).
+		rule(r2).
+		rule(r3).
+		rule(r4).
+		rule(constraint).
+		% a.
+		head(r0,a).
+		% b :- not c, not e.
+		head(r1,b).
+		neg(r1,c).
+		neg(r1,e).
+		% c :- not b, not e.
+		head(r2,c).
+		neg(r2,b).
+		neg(r2,e).
+		% d :- not e.
+		head(r3,d).
+		neg(r3,e).
+		% e :- not d.
+		head(r4,e).
+		neg(r4,d).
+		% :- a, b, d.
+		pos(constraint,a).
+		pos(constraint,b).
+		pos(constraint,d).
+	)");
+
+	EXPECT_EQ("Solutions:\n"
+	          "\n"
+	          "┣━ [1] a c constraint d r0 r1 r2 r3 r4 \n"
+	          "┗━ [1] a constraint e r0 r1 r2 r3 r4 \n",
+	          result);
 }
